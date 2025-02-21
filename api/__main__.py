3171ce150fcfe0fffe61aca74bf67afbf858d780
meliodas0n <mrunalnarayana@gmail.com>
2025-02-21 12:51:16 +0000

    moved to __main__

--- api/__main__.py
+++ api/__main__.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+import asyncio, uvicorn
 
-app = FastAPI()
+async def app():
+    config = uvicorn.Config("main:app", port=8000, host="lostvayne", log_level="info", reload=True)
+    server = uvicorn.Server(config)
+    await server.serve()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+if __name__ == "__main__":
+    asyncio.run(app())
